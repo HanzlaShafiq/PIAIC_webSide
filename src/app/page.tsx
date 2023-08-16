d926a454622fe19917ad74b4a73b73de8ceb1f45
Hanzla Shafiq <hanzlashafiq99@gmail.com>
2023-08-16 10:26:12 +0500

    fist

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,86 +6,47 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <>
+    
+    <div className='bg-gradient-to-r  from-purple-500 to-pink-500'>
+    <div className="menu flex justify-around bg-gradient-to-r  from-pink-500 to-purple-500 text-2xl mx-3 pt-3
+     rounded-full p-3 " >
+    <a href="/">Home</a>
+    <a href="/components"> About</a>
+    <a href="/web3"> Web3.0</a>
+    <button className='bg-pink-500 rounded-full w-28  animate-bounce' >
+    <a href="https://portal.piaic.org/signup" className=' font-bold text-slate-800'>APPLY</a>
+    </button>
+    
+  </div>
+  
+  
+  <section>
+  <div className='bg-gradient-to-r  from-purple-500 to-pink-500'>
+      <h1 className=' animate-pulse from-text-slate-600 drop-shadow-2xl to-text-slate-50 text-8xl my-0 p-32 font-autography text-center'>Launch Your Dev Career With Web 3 and Metaverse</h1>
+      <p className=' text-2xl  mb-8 pb-0  space-y-0 text-center' >Certified Web 3.0 and Metaverse Developer. A One and Quarter Years Panaverse DAO Earn as you Learn Program Getting Ready 
+        for the Next Generation of the Internet.</p>
+    </div>
+    </section>
+    <div className="box-border h-auto w-lg p-4  bg-slate-600">
+  <div className="menu flex justify-around  text-gray-300 text-2xl  
+     rounded-full p-3 wrapper " >
+    <p className="font-autography text-2xl my-auto">HANZLA SHAFIQ <br/> Front end Developer </p>
+    <div className=" my-auto">
+    <a href="hanzlashafiq99@gmail.com" >hanzlashafiq99@gamil.com</a>
+    <br />
+    <a href="https://wa.me/923040666355/">+923040666355</a>
+    </div>
+     <img src="./personal.jpg"
+    className="rounded h-32 w-32" alt="Hanzla Shafiq" /> 
+     
+    
+    
+  </div>
+  
+</div>
+</div>
+    
+      </>
   )
 }
---
+++ src/app/web3/page.tsx
@@ -0,0 +1,212 @@
+export default function Bio(){
+    return(
+     
+      <div className='bg-gradient-to-r from-purple-500 to-pink-500'>
+    <div className="menu flex justify-around bg-gradient-to-r  from-pink-500 to-purple-500  text-2xl  
+     rounded-full p-3 wrapper " >
+    <a href="/">Home</a>
+    <a href="/components"> About</a>
+    <a href="/web3"> Web3</a>
+    <button className='bg-pink-500 rounded-full w-28  animate-bounce' >
+    <a href="https://portal.piaic.org/signup" className=' font-bold text-slate-800'>APPLY</a>
+    </button>
+    
+    
+  </div>
+  
+  
+    
+    <section >
+    <div >
+      <div  >
+     <h1 className="text-bold text-center  underline  text-6xl">Web 3.0</h1>
+     <p className="text-center max-w-md mx-auto m-8 font-thin  overflow-hidden md:max-w-2xl text-center  items-center">Certified Web 3.0 and Metaverse Developer. 
+      A One and Quarter Years Panaverse DAO Earn as you Learn Program Getting Ready for the Next Generation of the Internet.</p>
+      <p className="text-center max-w-md text-1xl mx-auto mt-12 my-8xl m-12 overflow-hidden md:max-w-2xl text-center  items-center"> 
+      This curriculum is intended for beginners who want to learn software development from the ground up. The first two quarters are shared by all specialties and are dedicated to studying Object-Oriented Programming and cutting-edge Full-Stack Web 2.0 development. It is going to be a fifteen-month-long hybrid program that includes both onsite and online classes and is divided into five quarters of 13 weeks each. The emphasis will be on hands-on learning by educating students to produce projects. To accommodate everyone, courses will be held primarily on weekends or after 6:00 p.m. (Pakistan Time) on weekdays.
+         It employs a hybrid teaching format, with core onsite classes complemented by online Zoom laboratories and recorded videos.</p>
+         </div>
+         <div className="shadow-2xl bg-purple-500" >
+        <h1 className=" text-bold text-center  underline  text-6xl">Program Structure</h1>
+        <p className="text-center max-w-md text-1xl mx-auto mt-12  my-8xl m-12 overflow-hidden md:max-w-2xl text-center  items-center">A One and Quarter Years Panaverse DAO Earn as you Learn Program Getting Ready for the Next Generation of the Internet Consolidating Web 3.0,
+           Metaverse, Artificial Intelligence (AI), Cloud, Edge, and Ambient Computing/IoT Technologies</p>
+           </div>
+           <div>
+            <h1 className="text-bold text-left  mx-4  text-4xl">Core Courses</h1>
+            <p className="mx-6">Every participant of the program will start by completing the following three core courses:</p>
+           </div>
+
+           <div className="  p-2   max-w-sm my-4 mx-auto bg-gradient-to-r from-pink-400 to-purple-400 rounded overflow-hidden shadow-2xl">
+  
+  <div className="px-6 shadow-2xl py-4 mx-2">
+    <h1 className="font-bold text-xl mb-2">Quarter I</h1>
+    <p className="text-gray-900 text-base">
+    CS-101: Object-Oriented Programming using TypeScript
+    </p>
+  </div>
+  
+  
+</div>
+<div className="  p-2   max-w-sm my-4 mx-auto bg-gradient-to-r from-pink-400 to-purple-400 rounded overflow-hidden shadow-2xl">
+  
+  <div className="px-6 shadow-2xl py-4 mx-2">
+    <h1 className="font-bold text-xl mb-2">Quarter II</h1>
+    <p className="text-gray-900 text-base">
+    W2-201: Developing Planet-Scale Web 2.0 Serverless Cloud Cloud Apps and APIs
+     using Next.js 13 and Cloud Development Kit (CDK) for Terraform
+    </p>
+  </div>
+  
+  
+</div>
+<div className="  p-2   max-w-sm my-4 mx-auto bg-gradient-to-r from-pink-400 to-purple-400 rounded overflow-hidden shadow-2xl">
+  
+  <div className="px-6 shadow-2xl py-4 mx-2">
+    <h1 className="font-bold text-xl mb-2">Quarter III</h1>
+    <p className="text-gray-900 text-base">
+    $-101: Dollar Making Bootcamp - Full-Stack Template and API Product Development
+    </p>
+  </div>
+  
+  
+</div>
+<div>
+            <h1 className="text-bold text-left my-6 mx-4  text-4xl">Specialized Tracks</h1>
+            <p className="mx-6 my-4">After completing the first two quarters the participants will select one or more specializations consisting of two courses each:</p>
+           </div>
+           <div className="shadow-2xl bg-purple-500" >
+        <h1 className=" text-bold text-center  underline my-6 text-4xl">Web 3.0 (Blockchain) and Metaverse Specialization</h1>
+        <p className="text-center max-w-md text-1xl mx-auto mt-12  my-8xl m-12 overflow-hidden md:max-w-2xl text-center  items-center">
+        This Web 3.0 and Metaverse specialization focuses on developing full-stack Web 3.0 and Metaverse experiences for the next generation of the internet by specializing in
+         building worlds that merge the best of cutting-edge decentralized distributed blockchains with 3D metaverse client experiences.</p>
+           </div>
+
+           
+<div className="  p-2   max-w-sm my-4 mx-auto bg-gradient-to-r from-pink-400 to-purple-400 rounded overflow-hidden shadow-2xl">
+  
+  <div className="px-6 shadow-2xl py-4 mx-2">
+    <h1 className="font-bold text-xl mb-2">Quarter IV</h1>
+    <p className="text-gray-900 text-base">
+    W3-351: Developing Smart Contracts and Planet-Scale Web 3.0 Dapps
+    </p>
+  </div>
+  
+  
+</div>
+<div className="  p-2   max-w-sm my-4 mx-auto bg-gradient-to-r from-pink-400 to-purple-400 rounded overflow-hidden shadow-2xl">
+  
+  <div className="px-6 shadow-2xl py-4 mx-2">
+    <h1 className="font-bold text-xl mb-2">Quarter V</h1>
+    <p className="text-gray-900 text-base">
+    MV-361: Developing Planet-Scale Open Virtual and Augmented Metaverse Experiences
+
+    </p>
+  </div>
+  
+  
+
+</div>
+<div className="shadow-2xl bg-purple-500" >
+        <h1 className=" text-bold text-center  underline my-6 text-4xl">Artificial Intelligence (AI) and Deep Learning Specialization</h1>
+        <p className="text-center max-w-md text-1xl mx-auto mt-12  my-8xl m-12 overflow-hidden md:max-w-2xl text-center  items-center">
+        The AI and Deep Learning specialization focuses on building and deploying intelligent APIs using OpenAI models and building custom Deep Learning Tensorflow models.
+</p>
+           </div>
+           <div className="  p-2   max-w-sm my-4 mx-auto bg-gradient-to-r from-pink-400 to-purple-400 rounded overflow-hidden shadow-2xl">
+  
+  <div className="px-6 shadow-2xl py-4 mx-2">
+    <h1 className="font-bold text-xl mb-2">Quarter V</h1>
+    <p className="text-gray-900 text-base">
+    AI-351: Developing Planet-Scale Intelligent APIs and Python Programming</p>
+  </div>
+  
+  
+
+</div>
+<div className="  p-2   max-w-sm my-4 mx-auto bg-gradient-to-r from-pink-400 to-purple-400 rounded overflow-hidden shadow-2xl">
+  
+  <div className="px-6 shadow-2xl py-4 mx-2">
+    <h1 className="font-bold text-xl mb-2">Quarter V</h1>
+    <p className="text-gray-900 text-base">
+    AI-361: Deep Learning and MLOps </p>
+  </div>
+  
+</div>
+<div className="shadow-2xl bg-purple-500" >
+        <h1 className=" text-bold text-center  underline my-6 text-4xl">Cloud-Native Computing Specialization</h1>
+        <p className="text-center max-w-md text-1xl mx-auto mt-12  my-8xl m-12 overflow-hidden md:max-w-2xl text-center  items-center">
+        The Cloud-Native Computing Specialization focuses on Containers, Kubernetes, and CDK for Kubernetes. 
+
+</p>
+           </div>
+           <div className="  p-2   max-w-sm my-4 mx-auto bg-gradient-to-r from-pink-400 to-purple-400 rounded overflow-hidden shadow-2xl">
+  
+  <div className="px-6 shadow-2xl py-4 mx-2">
+    <h1 className="font-bold text-xl mb-2">Quarter V</h1>
+    <p className="text-gray-900 text-base">
+    CN-351: Certified Kubernetes Application Developer (CKAD)</p>
+  </div>
+  
+  
+
+</div>
+<div className="  p-2   max-w-sm my-4 mx-auto bg-gradient-to-r from-pink-400 to-purple-400 rounded overflow-hidden shadow-2xl">
+  
+  <div className="px-6 shadow-2xl py-4 mx-2">
+    <h1 className="font-bold text-xl mb-2">Quarter V</h1>
+    <p className="text-gray-900 text-base">
+    CN-361: Developing Multi-Cloud APIs using CDK for Terraform
+</p>
+  </div>
+  
+</div><div className="shadow-2xl bg-purple-500" >
+        <h1 className=" text-bold text-center  underline my-6 text-4xl">Ambient Computing and IoT Specialization</h1>
+        <p className="text-center max-w-md text-1xl mx-auto mt-12  my-8xl m-12 overflow-hidden md:max-w-2xl text-center  items-center">
+        The Ambient Computing and IoT Specialization focuses on building Smart Homes, Offices,
+         Factories, and Cities using Voice computing, Matter Protocol, and Embedded Devices. 
+</p>
+           </div>
+           <div className="  p-2   max-w-sm my-4 mx-auto bg-gradient-to-r from-pink-400 to-purple-400 rounded overflow-hidden shadow-2xl">
+  
+  <div className="px-6 shadow-2xl py-4 mx-2">
+    <h1 className="font-bold text-xl mb-2">Quarter V</h1>
+    <p className="text-gray-900 text-base">
+    AC-351: Ambient Computing with Voice Assistants and Matter Protocol Devices</p>
+  </div>
+  
+  
+
+</div>
+<div className="  p-2   max-w-sm my-4 mx-auto bg-gradient-to-r from-pink-400 to-purple-400 rounded overflow-hidden shadow-2xl">
+  
+  <div className="px-6 shadow-2xl py-4 mx-2">
+    <h1 className="font-bold text-xl mb-2">Quarter V</h1>
+    <p className="text-gray-900 text-base">
+    AC-361: Embedded Programming using C and Rust</p>
+  </div>
+  
+</div>
+<div className="box-border h-auto w-lg p-4  bg-slate-600">
+  <div className="menu flex justify-around  text-gray-300 text-2xl  
+     rounded-full p-3 wrapper " >
+    <p className="font-autography text-2xl my-auto">HANZLA SHAFIQ <br/> Front end Developer </p>
+    <div className=" my-auto">
+    <a href="hanzlashafiq99@gmail.com" >hanzlashafiq99@gamil.com</a>
+    <br />
+    <a href="https://wa.me/923040666355/">+923040666355</a>
+    </div>
+    <img src="./personal.jpg"
+    className="rounded h-32 w-32" alt="Hanzla Shafiq" />
+    
+    
+  </div>
+  
+</div>
+
+      </div>
+  </section>
+  </div>
+  
+  
+    )
+}
